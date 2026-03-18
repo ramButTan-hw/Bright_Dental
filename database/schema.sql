5bc6aa30f858b69ddf85b3179341cd5d5e010c0d
dillianemia <83387305+dillianemia@users.noreply.github.com>
2026-03-18 13:12:14 -0500

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,3 +1,25 @@
+CREATE TABLE IF NOT EXISTS users (
+    user_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_username VARCHAR(50) UNIQUE NOT NULL,
+    user_password VARCHAR(50) NOT NULL,
+    user_email VARCHAR(50) UNIQUE NOT NULL,
+    user_phone VARCHAR(20) UNIQUE NOT NULL,
+    account_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    account_last_login TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_role ENUM(
+        "ADMIN",
+        "PATIENT",
+        "DOCTOR",
+        "RECEPTIONIST",
+        "NURSE"
+    ) NOT NULL,
+    demographics_id INTEGER NOT NULL,
+    portal_last_login TIMESTAMP,
+    is_deleted TINYINT NOT NULL DEFAULT 0,
+    UNIQUE(user_id),
+    UNIQUE(user_email),
+    UNIQUE(user_username)
+);
 
 
 CREATE TABLE IF NOT EXISTS patients (
