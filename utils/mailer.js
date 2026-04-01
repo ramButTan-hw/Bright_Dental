7c2f06c1d8902a73c2e683e903722450c8720550
Jintian Wu <dumbzdumbz222@gmail.com>
2026-03-31 20:40:08 -0500

    outlook smtp

--- utils/mailer.js
+++ utils/mailer.js
@@ -7,6 +7,9 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER || '',
     pass: process.env.EMAIL_PASS || ''
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
