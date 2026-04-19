7411b68f12867ce724cc6a1373b5e134971c5d67
Jintian Wu <dumbzdumbz222@gmail.com>
2026-04-18 21:03:37 -0500

    Create FAQPage.jsx

---
+++ clinic-medical/src/pages/FAQPage.jsx
@@ -0,0 +1,10 @@
+function FAQPage() {
+  return (
+    <main style={{ maxWidth: '800px', margin: '2rem auto', padding: '0 1rem' }}>
+      <h1>Frequently Asked Questions</h1>
+      <p>FAQ content coming soon.</p>
+    </main>
+  );
+}
+
+export default FAQPage;
